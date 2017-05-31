1b2e3c30dcbedb32bfe6efec88462807ac37e703
Pravallika Rikhala <rpraval1@binghamton.edu>
2017-05-30 20:49:05 -0400

    added navbar / app structure

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Container } from 'semantic-ui-react'
+import ChatApp from './components/ChatApp'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container>
+          <ChatApp />
+        </Container>
       </div>
     );
   }
---
+++ src/components/ChatApp.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ChatApp extends Component {
+    render() {
+        return (
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default ChatApp;
